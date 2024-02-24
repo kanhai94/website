787dd0790e32cac5e335e65ab48ec7c83ad160ee
kanhai94 <kanhai94300@gmail.com>
2024-02-25 01:25:37 +0530

    2nd card read more

--- frontend/src/Component/HeaderCard/CardSecond.js
+++ frontend/src/Component/HeaderCard/CardSecond.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { Box, Button, Text, Flex,SimpleGrid} from "@chakra-ui/react";
+import { Box, Button, Text, Flex, SimpleGrid } from "@chakra-ui/react";
+import { Link } from "react-router-dom"; // Import Link component
+
 import {
   Card,
   CardBody,
@@ -10,12 +12,14 @@ import {
   Divider,
   ButtonGroup,
 } from "@chakra-ui/react";
+import { MdPersonalInjury } from "react-icons/md";
 
-const CardFirst = () => {
-  const header = "Areas of Practice";
+const CardSecond = () => {
+  const header = "Read More. To Know More.";
   return (
-    
     <Box style={{ position: "relative" }} textAlign={"center"}>
+        
+
       {/* Apply styles to the span elements */}
       <Text
         style={{
@@ -28,41 +32,260 @@ const CardFirst = () => {
         {header}
       </Text>
       {/* <Flex> */}
-      <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(400px, 1fr))'>
+      <SimpleGrid
+        spacing={3}
+        templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+      >
+        <Card variant={"unstyled"}>
+          <CardBody mt={2}>
+            <Box style={{ marginTop: "20px" }}>
+            
+              <Image
+                src="https://bolsterlegal.com/wp-content/uploads/2023/06/AMERICAN-BANK-TRUST-Co.-V.-COMMUNITY-HOSPITAL-1.png"
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+            </Box>
 
+            <Box
+              style={{
+                border: "10px",
+                border: "0.1rem inset black", 
+                marginTop: "20px",
+                // boxShadow: "0 0 0 2rem skyblue",
+                // outline: "0.5rem solid khaki",
+                padding: "10px",
+              }}
+            >
+              <Text style={{ fontWeight: "900", marginBottom: "19px" }}>
+                Santa Clara Valley Transportation Authority et al., v. Valley
+                Recycling
+              </Text>
+              <Text style={{ boder: "10px solid black", marginBottom: "25px" }}>
+                The principles of res judicata and collateral estoppel play
+                significant
+              </Text>
+
+              <Link to="/appointment">
+                <Button colorScheme="teal" mb={3}>
+                  Read More
+                </Button>
+                {/* */}
+              </Link>
+            </Box>
         
+
+      
+{/* 2nd box */}
+
+            <Box style={{ marginTop: "20px"}}>       
+            <Image
+              src="https://bolsterlegal.com/wp-content/uploads/2023/06/Michener-v.-Hutton-The-Principle-of-Res-ipsa-loquitur.png"
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+            />
+          </Box>
+          <Box
+            style={{
+              border: "10px",
+              border: "0.1rem inset black", 
+              marginTop: "20px",
+              // boxShadow: "0 0 0 2rem skyblue",
+              // outline: "0.5rem solid khaki",
+              padding: "10px",
+            }}
+          >
+            <Text style={{ fontWeight: "900", marginBottom: "19px" }}>
+            Michener v. Hutton- The Principle of Res ipsa loquitur
+            </Text>
+            <Text style={{ boder: "10px solid black", marginBottom: "25px" }}>
+            In Michener v. Hutton, the plaintiff invoked the legal doctrine of res i
+            </Text>
+
+            <Link to="/appointment">
+              <Button colorScheme="teal" mb={3}>
+                Read More
+              </Button>
+              {/* */}
+            </Link>
+          </Box>
+
+
+{/* 3rd box */}
+
+<Box style={{ marginTop: "20px"}}>          
+            <Image
+              src="https://bolsterlegal.com/wp-content/uploads/2023/05/Dee-Ann-Abelaret-al.v.-Jeffrey-Mora-Professional-Medical-Negligence_Bolster-Legal.png"
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+            />
+          </Box>
+          <Box
+            style={{
+              border: "10px",
+              border: "0.1rem inset black", 
+              marginTop: "20px",
+              // boxShadow: "0 0 0 2rem skyblue",
+              // outline: "0.5rem solid khaki",
+              padding: "10px",
+            }}
+          >
+            <Text style={{ fontWeight: "900", marginBottom: "19px" }}>
+            Dee Ann Abelaret al.v. Jeffrey Mora- Professional Medical Negligence
+            </Text>
+            <Text style={{ boder: "10px solid black", marginBottom: "25px" }}>
+            Are you interested in learning about the intricate details of personal i
+            </Text>
+
+            <Link to="/appointment">
+              <Button colorScheme="teal" mb={3}>
+                Read More
+              </Button>
+              {/* */}
+            </Link>
+          </Box>
         
-      <Card >
-        <CardBody>
-          
-          <Image
-            src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            alt="Green double couch with wooden legs"
-            borderRadius="lg"
-          />
-
-          <Text>
-            Personal Injury
-          </Text>
-         
-
-         <box style={{ boder: "2px solid black" }}>
-         Santa Clara Valley Transportation Authority et al., v. Valley Recycling
-
-
-         </box>
-        </CardBody>
-
-
-
-        
-        
-      </Card>
-      {/* </Flex> */}
+          </CardBody>
+        </Card>
+        {/* </Flex> */}
       </SimpleGrid>
+  
     </Box>
-    
-
   );
 };
-export default CardFirst;
+export default CardSecond;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { Box, Button, Text, Flex, VStack } from "@chakra-ui/react";
+// import { Link } from "react-router-dom";
+// import { Card, CardBody, Image } from "@chakra-ui/react";
+
+// const CardSecond = () => {
+//   const header = "Read More. To Know More.";
+//   return (
+//     <Box style={{ position: "relative" }} textAlign={"center"}>
+//       <Text
+//         style={{
+//           marginTop: "70px",
+//           fontSize: "35px",
+//           fontWeight: "bold",
+//           top: "30%",
+//         }}
+//       >
+//         {header}
+//       </Text>
+//       <Flex
+//         justify="center"
+//         direction={{ base: "column", md: "row" }} // Stacks cards vertically on mobile, horizontally on larger screens
+//       >
+//         <Card variant={"unstyled"} style={{ marginBottom: { base: "20px", md: "0" }, marginRight: { md: "5px" } }}>
+//           <CardBody mt={2}>
+//             <Box style={{ marginTop: "20px" }}>
+//               <Image
+//                 src="https://bolsterlegal.com/wp-content/uploads/2023/06/AMERICAN-BANK-TRUST-Co.-V.-COMMUNITY-HOSPITAL-1.png"
+//                 alt="Green double couch with wooden legs"
+//                 borderRadius="lg"
+//               />
+//             </Box>
+//             <Box
+//               style={{
+//                 border: "10px",
+//                 border: "0.1rem inset black",
+//                 marginTop: "20px",
+//                 padding: "10px",
+//               }}
+//             >
+//               <Text style={{ fontWeight: "900", marginBottom: "19px" }}>
+//                 Santa Clara Valley Transportation Authority et al., v. Valley Recycling
+//               </Text>
+//               <Text style={{ marginBottom: "25px" }}>
+//                 The principles of res judicata and collateral estoppel play significant
+//               </Text>
+//               <Link to="/appointment">
+//                 <Button colorScheme="teal" mb={3}>
+//                   Read More
+//                 </Button>
+//               </Link>
+//             </Box>
+//           </CardBody>
+//         </Card>
+//         <Card variant={"unstyled"} style={{ marginBottom: { base: "20px", md: "4" }, marginRight: { md: "20px" } }}>
+//           <CardBody mt={2}>
+//             <Box style={{ marginTop: "20px" }}>
+//               <Image
+//                 src="https://bolsterlegal.com/wp-content/uploads/2023/06/Michener-v.-Hutton-The-Principle-of-Res-ipsa-loquitur.png"
+//                 alt="Green double couch with wooden legs"
+//                 borderRadius="lg"
+//               />
+//             </Box>
+//             <Box
+//               style={{
+//                 border: "10px",
+//                 border: "0.1rem inset black",
+//                 marginTop: "20px",
+//                 padding: "10px",
+//               }}
+//             >
+//               <Text style={{ fontWeight: "900", marginBottom: "19px" }}>
+//                 Michener v. Hutton- The Principle of Res ipsa loquitur
+//               </Text>
+//               <Text style={{ marginBottom: "25px" }}>
+//                 In Michener v. Hutton, the plaintiff invoked the legal doctrine of res ipsa loquitur.
+//               </Text>
+//               <Link to="/appointment">
+//                 <Button colorScheme="teal" mb={3}>
+//                   Read More
+//                 </Button>
+//               </Link>
+//             </Box>
+//           </CardBody>
+//         </Card>
+//         <Card variant={"unstyled"} style={{ marginBottom: { base: "20px", md: "0" } }}>
+//           <CardBody mt={2}>
+//             <Box style={{ marginTop: "20px" }}>
+//               <Image
+//                 src="https://bolsterlegal.com/wp-content/uploads/2023/05/Dee-Ann-Abelaret-al.v.-Jeffrey-Mora-Professional-Medical-Negligence_Bolster-Legal.png"
+//                 alt="Green double couch with wooden legs"
+//                 borderRadius="lg"
+//               />
+//             </Box>
+//             <Box
+//               style={{
+//                 border: "10px",
+//                 border: "0.1rem inset black",
+//                 marginTop: "20px",
+//                 padding: "10px",
+//               }}
+//             >
+//               <Text style={{ fontWeight: "900", marginBottom: "19px" }}>
+//                 Dee Ann Abelaret al.v. Jeffrey Mora- Professional Medical Negligence
+//               </Text>
+//               <Text style={{ marginBottom: "25px" }}>
+//                 Are you interested in learning about the intricate details of personal injury cases?
+//               </Text>
+//               <Link to="/appointment">
+//                 <Button colorScheme="teal" mb={3}>
+//                   Read More
+//                 </Button>
+//               </Link>
+//             </Box>
+//           </CardBody>
+//         </Card>
+//       </Flex>
+//     </Box>
+//   );
+// };
+
+// export default CardSecond;
--- frontend/src/Component/Home/Home.js
+++ frontend/src/Component/Home/Home.js
@@ -6,31 +6,28 @@ import Header from "../Header/HeaderFirst";
 import HeaderSecond from "../HeaderBody";
 import CardFirst from "../HeaderCard/CardFirst";
 import DataShow from "../AchivementDataShow/Datashow";
-import CardSecond from  "../HeaderCard/CardSecond";
+import CardSecond from "../HeaderCard/CardSecond";
 
 function Home() {
   return (
     <Box>
       {/* components */}
-      {/* <NavbarTop /> */}
-      {/* <NavbarTopFirst /> */}
+      <NavbarTop />
+      <NavbarTopFirst />
       <Header />
-      <Box w="80%" ml='auto' mr="auto">
+      <Box w="80%" ml="auto" mr="auto">
         <HeaderSecond />
-       <CardFirst />
-        <DataShow/>
-        <CardSecond/>
+        <CardFirst />
+        <DataShow />
+        <CardSecond />
       </Box>
-      
+
       <Box bg="green">
         {/* <Center> */}
         {/* <Twocards/> */}
 
         {/* </Cente r> */}
-
-
       </Box>
-
     </Box>
   );
 }
